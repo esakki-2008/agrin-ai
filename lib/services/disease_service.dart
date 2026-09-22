0f87df955277c5a440611e52590e31d379233ba6
ESAKKI RAJA SALAIKUMAR <esakkiraja2528@gmail.com>
2026-09-22 17:08:17 +0530

    Wire disease service to deployment API

--- lib/services/disease_service.dart
+++ lib/services/disease_service.dart
@@ -54,7 +54,7 @@ class DiseaseAction {
 }
 
 class DiseaseService {
-  static const String baseUrl = 'http://127.0.0.1:8000';
+  static String get baseUrl => ApiConfig.baseUrl;
 
   Future<DiseaseAnalysis> analyze({
     required Uint8List imageBytes,
