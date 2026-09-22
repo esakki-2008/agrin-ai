974a485848efe4f49199e9bfe8c09feeeb4b6b40
ESAKKI RAJA SALAIKUMAR <esakkiraja2528@gmail.com>
2026-09-22 08:48:07 +0530

    Keep editorial theme compatible with Flutter 3.32

--- lib/theme/agri_n_design.dart
+++ lib/theme/agri_n_design.dart
@@ -72,7 +72,7 @@ class AgriNDesign {
         ),
       ),
       dividerTheme: const DividerThemeData(color: line, thickness: 1, space: 1),
-      cardTheme: CardThemeData(
+      cardTheme: CardTheme(
         color: paper,
         elevation: 0,
         margin: EdgeInsets.zero,
