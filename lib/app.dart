062e26fae15f0e75a8a26a6404c7702ffc3dca66
ESAKKI RAJA SALAIKUMAR <esakkiraja2528@gmail.com>
2026-09-22 17:40:01 +0530

    Add Farm Digital Twin route

--- lib/app.dart
+++ lib/app.dart
@@ -8,6 +8,7 @@ import 'features/regenerative_farming/regenerative_farming_page.dart';
 import 'features/historical_intelligence/historical_intelligence_page.dart';
 import 'features/brics_network/brics_network_page.dart';
 import 'features/intelligence_agent/intelligence_agent_page.dart';
+import 'features/farm_digital_twin/farm_digital_twin_page.dart';
 import 'theme/agri_n_design.dart';
 
 class AgriNApp extends StatelessWidget {
@@ -25,6 +26,7 @@ class AgriNApp extends StatelessWidget {
         GoRoute(path: '/historical-intelligence', builder: (context, state) => const HistoricalIntelligencePage()),
         GoRoute(path: '/brics-network', builder: (context, state) => const BricsNetworkPage()),
         GoRoute(path: '/intelligence-agent', builder: (context, state) => const IntelligenceAgentPage()),
+        GoRoute(path: '/farm-digital-twin', builder: (context, state) => const FarmDigitalTwinPage()),
       ],
     );
 
