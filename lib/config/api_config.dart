8b480fdddf48a031b8e09b793026abdd29335973
ESAKKI RAJA SALAIKUMAR <esakkiraja2528@gmail.com>
2026-09-23 08:52:28 +0530

    feat: make API configuration platform aware

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -15,6 +15,23 @@ class ApiConfig {
       return 'http://127.0.0.1:8000';
     }
 
-    return 'http://127.0.0.1:8000';
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        // Android Emulator cannot reach the host machine through localhost.
+        // For a physical Android device, pass --dart-define=AGRIN_API_URL=...
+        return 'http://10.0.2.2:8000';
+      case TargetPlatform.iOS:
+      case TargetPlatform.macOS:
+      case TargetPlatform.windows:
+      case TargetPlatform.linux:
+        return 'http://127.0.0.1:8000';
+      case TargetPlatform.fuchsia:
+        return 'http://127.0.0.1:8000';
+    }
+  }
+
+  static Uri endpoint(String path) {
+    final normalized = path.startsWith('/') ? path : '/$path';
+    return Uri.parse('$baseUrl$normalized');
   }
 }
